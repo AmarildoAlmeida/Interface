bcc5ae41e7f5be2e7874004306e7e70d928d7d3c
AmarildoAlmeida <amarildo.almeida.s@gmail.com>
2024-11-20 13:24:16 -0400

    aULA 17

--- src/containers/Register/index.jsx
+++ src/containers/Register/index.jsx
@@ -1,11 +1,11 @@
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import {toast} from 'react-toastify';
+import { toast } from 'react-toastify';
 import * as yup from 'yup';
 
 import Logo from '../../assets/logo.svg';
 import { Button } from '../../components/Button';
-import {api} from '../../services/api';
+import { api } from '../../services/api';
 import {
   Container,
   Form,
@@ -18,22 +18,22 @@ import {
 export function Register() {
   const schema = yup
     .object({
-      name:yup.string().required('O nome é obrigatório'),
-     email: yup
-     .string()
-     .email('Digite um E-mail válido')
-     .required('O E-mail é obrigatório'),
-      password:yup
-      .string()
-      .min(6, 'A senha deve ter 6 caracteres')
-      .required('Digite uma senha'),
+      name: yup.string().required('O nome é obrigatório'),
+      email: yup
+        .string()
+        .email('Digite um E-mail válido')
+        .required('O E-mail é obrigatório'),
+      password: yup
+        .string()
+        .min(6, 'A senha deve ter 6 caracteres')
+        .required('Digite uma senha'),
       confirmPassword: yup
-      .string()
-      .oneOf([yup.ref('password')], 'As senhas devem ser iguais ')
-      .required('Confirme sua senha'),
+        .string()
+        .oneOf([yup.ref('password')], 'As senhas devem ser iguais ')
+        .required('Confirme sua senha'),
     })
     .required();
-  
+
   const {
     register,
     handleSubmit,
@@ -44,20 +44,31 @@ export function Register() {
   console.log(errors);
 
   const onSubmit = async (data) => {
-    const response = await toast.promise(
-      api.post('/users',{
-        name:data.name,
-        email:data.email,
-        password:data.password,
-      }),
-      {
-        pending:'Verificando seus dados',
-        success: 'Cadastro efetuado com sucesso',
-        error: 'Ops, algo deu errado! Tente novamente',
-      }, 
-    );
- 
-    console.log(response);
+    try {
+      const { status } = await api.post(
+        '/users',
+        {
+          name: data.name,
+          email: data.email,
+          password: data.password,
+        },
+        {
+          validateStatus: () => true,
+        },
+      );
+      
+    
+      if (status === 200 || status === 201) {
+        toast.success('Conta criada com sucesso!');
+      } else if (status === 409) {
+        toast.error('Email já cadastrado! Faça o login para continuar');
+      } else {
+        throw new Error();
+      }
+     
+    } catch (error) {
+      toast.error('😞 Falha no sistema! Tente novamente');
+    }
   };
 
   return (
@@ -68,15 +79,15 @@ export function Register() {
       <RightContainer>
         <Title>Criar Conta</Title>
         <Form onSubmit={handleSubmit(onSubmit)}>
-        <InputContainer>
+          <InputContainer>
             <label> Nome </label>
             <input type="text" {...register('name')} />
-          <p>{errors?.name?.message}</p>
+            <p>{errors?.name?.message}</p>
           </InputContainer>
           <InputContainer>
             <label> Email </label>
             <input type="email" {...register('email')} />
-          <p>{errors?.email?.message}</p>
+            <p>{errors?.email?.message}</p>
           </InputContainer>
 
           <InputContainer>
@@ -87,7 +98,7 @@ export function Register() {
           <InputContainer>
             <label>Confirmar Senha</label>
             <input type="password" {...register('confirmPassword')} />
-          <p>{errors?.confirmPassword?.message}</p>
+            <p>{errors?.confirmPassword?.message}</p>
           </InputContainer>
           <Button type="submit">Criar Conta</Button>
         </Form>
